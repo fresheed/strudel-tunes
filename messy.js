5f2db410b6dd2b701fb067d7c1fa2972bca2ed58
fresheed <ok.john.rus@gmail.com>
2026-03-16 23:47:35 +0100

    added verse 1 of messy

--- messy.js
+++ messy.js
@@ -16,15 +16,29 @@ let riff_Dm = n("0@2 ~ 4 ~ 7 ~ 4 0@2 ~ 4 ~ 7 ~ 4").scale("D:minor").transpose(-2
 
 let riff_E1 = n("0@2 ~ 4 ~ 7 ~ 4").scale("E:major").transpose(-24);
 let riff_E2_slower = n("0@2 ~ [4 5@3] ~ [7 8@3] ~ 4").scale("E:major").transpose(-24);
-let riff_E2_faster = n("0 0 1 1 [4 5@3] 4 7 5").scale("E:major").transpose(-24);
+let riff_E2_slower_alt_high = n("0@2 ~ 4 ~ [7 8@2] ~ [10 11@2]").scale("E:major").transpose(-24);
+let riff_E2_faster = n("0 0 1 1 [4 5@2] 4 7 5").scale("E:major").transpose(-24);
+let riff_E2_even_slower = n("0@2 ~ 4 ~ [7 8@2] ~ 4").scale("E:major").transpose(-24);
 
 let intro = cat(riff_Dm, 
                 seq(riff_E1, riff_E2_slower),
                 riff_Dm, 
-                seq(riff_E1, riff_E2_faster))
+                seq(riff_E1, riff_E2_faster),
+              )
+
+let verse1 = cat(
+    /* riff_Dm, 
+    seq(riff_E1, riff_E2_even_slower),
+    riff_Dm, 
+    seq(riff_E1, riff_E2_even_slower), */
+    riff_Dm, 
+    seq(riff_E1, riff_E2_slower_alt_high),                
+    riff_Dm, 
+    seq(riff_E1, riff_E2_faster),
+)
 
 let bass = 
-  intro.slow(2).sound("gm_electric_bass_finger")
+  verse1.slow(2).sound("gm_electric_bass_finger")
 
 $: bass;
 $: drums;
