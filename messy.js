7b1a963c0ce0ce3c201775739e4f513f896c0eb1
fresheed <ok.john.rus@gmail.com>
2026-03-20 19:37:31 +0100

    wip

--- messy.js
+++ messy.js
@@ -2,26 +2,27 @@ let shift = 2
 
 let riff_Dm = n("0@2 ~ 4 ~ 7 ~ 4 0@2 ~ 4 ~ 7 ~ 4").scale("D:minor").transpose(-24 + shift);
 
-let riff_E1 = n("0@2 ~ 4 ~ 7 ~ 4").scale("E:major").transpose(-24 + shift);
-let riff_E2_l2l3 = n("0@2 ~ [4 5@3] ~ [7 8@3] ~ 4").scale("E:major").transpose(-24 + shift);
-let riff_E2_l3_h = n("0@2 ~ 4 ~ [7 8@3] ~ 7").scale("E:major").transpose(-24 + shift);
+let riff_E1 =        n("0@2 ~ 4 ~ 7 ~ 4").scale("E:major").transpose(-24 + shift);
+let riff_E2_l2l3 =   n("0@2 ~ [4 5@3] ~ [7 8@3] ~ 4").scale("E:major").transpose(-24 + shift);
+let riff_E2_l3 =     n("0@2 ~ 4 ~ [7 8@2] ~ 4").scale("E:major").transpose(-24 + shift);
+let riff_E2_l3_h =   n("0@2 ~ 4 ~ [7 8@3] ~ 7").scale("E:major").transpose(-24 + shift);
 let riff_E2_l3_l4h = n("0@2 ~ 4 ~ [7 8@2] ~ [10 11@2]").scale("E:major").transpose(-24 + shift);
 let riff_E2_faster = n("0 0 1 1 [4 5@2] 4 7 5").scale("E:major").transpose(-24 + shift);
-let riff_E2_l3 = n("0@2 ~ 4 ~ [7 8@2] ~ 4").scale("E:major").transpose(-24 + shift);
-
-// TODO: split, unify names
-let chorus_Dm4 = n("0@2 ~ 0 ~ 0 ~ 7   0@2 ~ 0 ~ 0 ~ 7").scale("D:minor").transpose(-24 + shift);
-let chorus_E3 = n("0@2 ~ 0 ~ 7 ~ 0   0@2 ~ 0!3 5 4").scale("E:major").transpose(-24 + shift);
-let chorus_Dm3 = n("0@2 ~ 0 ~ 7 ~ 0   0@2 ~ 0 ~ 7 ~ 0").scale("D:minor").transpose(-24 + shift);
-let chorus_E4 = n("0@2 ~ 0 ~ 0 ~ 7   0 0 4 0 7 6b 5 4").scale("E:major").transpose(-24 + shift);
-let chorus_E4_high = n("0@2 ~ 0 ~ 0 ~ 7   7 7 1 1 [4 5@2] 4 7 5").scale("E:major").transpose(-24 + shift);
-let chorus_E4_asc = n("0@2 ~ 0 ~ 0 ~ 7   0@2 ~ 0 1 4 5 7").scale("E:major").transpose(-24 + shift);
-let mk_E_riff = (e2) => seq(riff_E1, e2)
 
 // TODO: split into two parts and unify with riff
+let chorus_Dm4 =   n("0@2 ~ 0 ~ 0 ~ 7   0@2 ~ 0 ~ 0 ~ 7").scale("D:minor").transpose(-24 + shift);
+let chorus_Dm3 =   n("0@2 ~ 0 ~ 7 ~ 0   0@2 ~ 0 ~ 7 ~ 0").scale("D:minor").transpose(-24 + shift);
 let chorus_Dm_43 = n("0@2 ~ 0 ~ 0 ~ 7   0@2 ~ 0 ~ 7 ~ 0").scale("D:minor").transpose(-24 + shift);
 let chorus_Dm_34 = n("0@2 ~ 0 ~ 7 ~ 0   0@2 ~ 0 ~ 0 ~ 7").scale("D:minor").transpose(-24 + shift);
-let chorus_E_low = n("[0@2 ~ 0 ~ 0 ~ 0]!2").scale("E:major").transpose(-24 + shift);
+
+let chorus_E3 =      n("0@2 ~ 0 ~ 7 ~ 0   0@2 ~ 0!3 5 4").scale("E:major").transpose(-24 + shift);
+let chorus_E4_desc = n("0@2 ~ 0 ~ 0 ~ 7   0 0 4 0 7 6b 5 4").scale("E:major").transpose(-24 + shift);
+let chorus_E4_low = n("0@2 ~ 0 ~ 0 ~ 7    0 0 1 1 [4 5@2] 4 7 5").scale("E:major").transpose(-24 + shift);
+let chorus_E4_high = n("0@2 ~ 0 ~ 0 ~ 7   7 7 1 1 [4 5@2] 4 7 5").scale("E:major").transpose(-24 + shift);
+let chorus_E4_asc =  n("0@2 ~ 0 ~ 0 ~ 7   0@2 ~ 0 1 4 5 7").scale("E:major").transpose(-24 + shift);
+let chorus_E_low =   n("[0@2 ~ 0 ~ 0 ~ 0]!2").scale("E:major").transpose(-24 + shift);
+let mk_E_riff = (e2) => seq(riff_E1, e2)
+
 
 let intro = cat(riff_Dm, mk_E_riff(riff_E2_l2l3), 
                 riff_Dm, mk_E_riff(riff_E2_faster))
@@ -35,6 +36,7 @@ let chorus1 = cat(
 
 let chorus2 = cat(
   chorus_Dm4, chorus_E4_asc,
+  chorus_Dm4, chorus_E4_high,
 )
 
 
