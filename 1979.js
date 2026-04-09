dd0d2d996b5d6ef71800f0d9292357ba66db6a55
fresheed <ok.john.rus@gmail.com>
2026-04-09 16:29:33 +0200

    high-level structure of 1979

---
+++ 1979.js
@@ -0,0 +1,36 @@
+let main_riff = null
+let trans1 = null
+let trans2 = null
+
+// main_riff * 4
+// trans F high, A#
+// verse * 3
+// trans2 F low, A#
+// verse * 3
+// trans2 F low, A#
+// main_riff * 4
+// trans F low, G#
+// verse * 3
+// trans A#, A#, C, G#   * 3
+// trans F low, A#
+// silence * 6
+// main_riff * 1
+// verse * 2
+// trans F low, A#
+// verse * 3
+// trans F low, A#
+// main_riff * 2
+
+
+
+let bass = 
+  chorus1.slow(1)
+  .sound("gm_electric_bass_pick").lpf(900)
+
+let drums = sound(`
+  [bd, rim] sd bd sd
+`).bank("RolandTR909");
+
+setcpm(30);
+$: bass;
+$: drums;
