12e36a5e51702ee23869287fca74f571a7b27d93
fresheed <ok.john.rus@gmail.com>
2026-05-20 14:05:26 +0200

    wip make_me_wanna_die

--- make_me_wanna_die.js
+++ make_me_wanna_die.js
@@ -9,6 +9,20 @@ let verse_alt2 = n("[0!8]@2 2 4b").scale("F:minor").transpose(-24)
 
 let verse = cat(verse_16ths, verse_alt1, verse_16ths, verse_alt2)
 
+// same rhythm:
+// F, G#,  A#, C,  C# D#,  F D#, A#
+// end with: C, A#, G#, G 8ths, half pause
+let chorus = null
+
+// then pre-verse x2
+// then verse riff 1 (the end is slightly different)
+// then verse riff 2
+// there is some extra pre-chorus
+
+// chorus the same
+
+// interlude: whole notes, repeat 4 times: F, C#, G#, D# 
+
 let bass = 
   verse.slow(2)
   .sound("gm_electric_bass_finger").lpf(900)
