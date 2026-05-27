468b994fcee466cc613b6bb8afa486d20ed71f00
fresheed <ok.john.rus@gmail.com>
2026-05-28 00:12:30 +0200

    wip

--- bass/hump_de_bump.js
+++ bass/hump_de_bump.js
@@ -1,5 +1,8 @@
 let in_Dm = (notes) => n(notes).scale("D:minor").transpose(-12)
 let in_Bb7 = (notes) => n(notes).scale("Bb:mixolydian").transpose(-24)
+let in_A7 = (notes) => n(notes).scale("A:mixolydian").transpose(-24)
+let in_G7 = (notes) => n(notes).scale("G:mixolydian").transpose(-24)
+let in_C = (notes) => n(notes).scale("C:major").transpose(-12)
 
 // @2 is just to make it sound better here
 let riff_odd =    in_Dm("[[[0,4]@2 ~] ~] [~ [[0,4]@2 ~]] ~@2")
@@ -21,11 +24,25 @@ let pre_chorus_ad = in_Dm("[[[0,4]@2 ~] ~] [~ [[0,4]@2 ~]]   [~ [[0,5#]@2 ~]] [[
 // played twice
 let pre_chorus1 = cat(riff_odd, pre_chorus_asc, riff_odd, pre_chorus_ad)
 
-let chorus_Dm =  in_Dm("0 [7 6 4 6] [4#gain:2 3 ~@2] [2 3 ~@2]")
-let chorus_Bb7 = in_Bb7("0 [7 6 4 6] [4 3 ~@2] [2 3 ~@2]")
+let chorus_Dm =  in_Dm("0 [7 6 4 6] [4 3 ~@2] [2 3 ~@2]")
+let chorus_Bb7 = in_Bb7("0 [7 6# 5 6#] [5 4 ~@2] [2 4 ~@2]")
+// halves
+let chorus_A7 = in_A7("0 [7 6 4 3]")
+let chorus_G7 = in_G7("0 [7 6 4 3]")
+let chorus_B_short = in_Bb7("0 [0 2 3 4]")
+let chorus_C = in_C("0 [0 2 3 4]")
+let chorus_C_triplets = in_C("[0 [8 7 5]] [[5b 4] 3]")
+
+let chorus = cat(
+                //  chorus_Dm, 
+                //  chorus_Bb7,
+                //  fastcat(chorus_A7, chorus_G7),
+                //  fastcat(chorus_B_short, chorus_C),  
+                  fastcat(chorus_B_short, chorus_C_triplets),  
+)
  
 let bass = 
-  pre_chorus1.slow(1)
+  chorus.slow(1)
   .sound("gm_slap_bass_1").lpf(900)
 
 let drums = sound(`
